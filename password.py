76374a322b2fc60cc2ab30b875a75e804e40f3e3
Muturi403 <faith.muturi@student.moringaschool.com>
2021-08-01 23:50:50 +0300

    added user list

---
+++ password.py
@@ -0,0 +1,20 @@
+class User:
+
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = []
+    def __init__(self,first_name,second_name,username,password):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+
+        """
+        save_user method saves user objects into user_list
+        """
+
+        User.user_list.append(self)
